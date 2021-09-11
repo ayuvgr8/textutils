c0347e8f6c3ba40da659decbc89a0e1a45ea92b0
ayuvgr8 <ayuvgr8@gmail.com>
2021-09-11 07:09:24 +0530

    Third commit Lower Case Fn

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,12 +3,18 @@ import React, { useState } from "react";
 
 export default function TextForm(props) {
     const handleUpClick = () => {
-        console.log("Uppercase was Clicked" + text );
-        let newText= text.toUpperCase();
+       // console.log("Uppercase was Clicked" + text ); // just for the debugging("yahan pe "text" call ho rha hai")
+        let newText= text.toUpperCase();  // we made a variable to hold the new changes after using function uppercase 
         setText(newText);
     }
+
+     const handleUpClick2 = () => {
+       // console.log("Uppercase was Clicked" + text ); // just for the debugging("yahan pe "text" call ho rha hai")
+       let newText2 = text.toLowerCase();
+       setText(newText2);
+     };
     const handleOnChange = (event) => {
-      console.log("On Change");
+     // console.log("On Change"); // just for the debugging
       setText(event.target.value);
     };
     const [text, setText] = useState("Enter Text Here  ");
@@ -17,12 +23,14 @@ export default function TextForm(props) {
     // setText("New Text is Here"); //Correct way to set text
 
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+    <><div>
+          <h1>{props.heading}</h1>
+          <div className="mb-3">
+              <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+          </div>
+          <button className="mb-7 my-8   btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button>
+         <button className="mb-7 my-8  btn btn-primary" onClick={handleUpClick2}>Convert To Lowercase</button>  
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button> 
-    </div>
+     </>
   );
 }
