624588e1e2f0d5c3f557dc0afcb71a650481a7de
ayuvgr8 <ayuvgr8@gmail.com>
2021-09-14 05:44:28 +0530

    seventh commit(json  modified)

--- src/App.js
+++ src/App.js
@@ -43,12 +43,7 @@ function App() {
   return (
     <>
       <Router>
-        <Navbar
-          title="TextUtils"
-          mode={mode}
-          toggleMode={toggleMode}
-          key={new Date()}
-        />
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
         <Alert alert={alert} />
         <div className="container my-3">
           <Switch>
@@ -57,11 +52,7 @@ function App() {
               <About mode={mode} />
             </Route>
             <Route exact path="/">
-              <TextForm
-                showAlert={showAlert}
-                heading="Try TextUtils - word counter, character counter, remove extra spaces"
-                mode={mode}
-              />
+              <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />
             </Route>
           </Switch>
         </div>
